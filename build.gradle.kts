cb363367e2a66da75bbac201e50f8661a8aed3e3
dkim19375 <65050767+dkim19375@users.noreply.github.com>
2023-12-01 18:12:32 -0500

    Finish Day 1

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+import me.dkim19375.dkimgradle.util.setJavaVersion
+
+plugins {
+    kotlin("jvm") version "1.9.21"
+    id("io.github.dkim19375.dkim-gradle") version "1.3.7"
+}
+
+group = "me.dkim19375"
+
+setJavaVersion(JavaVersion.VERSION_17)
+
+repositories {
+    mavenCentral()
+}
